83f97ee47e0061fcada898434a3bd6bcf5ca5f3d
Justin Xia <justinxia9@gmail.com>
2021-09-10 18:12:09 -0400

    Add audio elements

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-const playButtons = document.querySelectorAll(".play");
+const playButtons = document.querySelectorAll('.play');
 let playing = false;
+
+// music
+const melody = new Audio('sounds/melody-of-nature.mp3');
+const acoustic = new Audio('sounds/acoustic-indie-folk.mp3');
+const cosmic = new Audio('sounds/cosmic-glow.mp3');
+const inspiration = new Audio('sounds/inspiration-piano.mp3');
+const moonlight = new Audio('sounds/moonlight-mastered.mp3');
+const lilly = new Audio('sounds/water-lilly.mp3');
+const happy = new Audio('sounds/happy-days.mp3');
+const hipHop = new Audio('sounds/hip-hop-lo-fi.mp3');
+const rainy = new Audio('sounds/rainy-day.mp3');
+const ocean = new Audio('sounds/ocean-diving-underwater.mp3');
+
+
 
 function changeButton() {
   playing = !playing;
@@ -10,4 +24,4 @@ function changeButton() {
   }
 }
 
-playButtons.forEach(button => button.addEventListener("click", changeButton));
+playButtons.forEach(button => button.addEventListener('click', changeButton));
