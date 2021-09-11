eff92823bc187906e51b6e5eeecc836d715617c0
Justin Xia <justinxia9@gmail.com>
2021-09-10 20:39:23 -0400

    Update play function

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const playButtons = document.querySelectorAll('.play');
-let playing = false;
+let play = false;
+
+const music = [new Audio('sounds/melody.mp3'), new Audio('sounds/acoustic.mp3'), new Audio('sounds/cosmic.mp3'),
+new Audio('sounds/inspiration.mp3'), new Audio('sounds/moonlight.mp3'), new Audio('sounds/lilly.mp3'),
+new Audio('sounds/happy.mp3'), new Audio('sounds/hipHop.mp3'), new Audio('sounds/rainy.mp3'),
+new Audio('sounds/ocean.mp3')];
 
 function changeButton() {
-  playing = !playing;
-  if(playing === true) {
+  play = !play;
+  if(play === true) {
     this.innerHTML = '❚❚';
   } else {
     this.innerHTML = '►';
@@ -11,8 +16,7 @@ function changeButton() {
 }
 
 function playSound() {
-  const song = new Audio(`sounds/${this.id}.mp3`)
-  console.log(song);
+  const song = music[this.id];
   song.play();
 }
 
