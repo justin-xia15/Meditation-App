e2905c4bae33a505345064a832bcde637a6a64c3
Justin Xia <justinxia9@gmail.com>
2021-09-11 11:55:53 -0400

    Fix play/pause button display

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const playButtons = document.querySelectorAll('.play');
-let play = false;
 let currentSong = null;
+let currentButton;
 
 const music = [new Audio('sounds/melody.mp3'), new Audio('sounds/acoustic.mp3'), new Audio('sounds/cosmic.mp3'),
 new Audio('sounds/inspiration.mp3'), new Audio('sounds/moonlight.mp3'), new Audio('sounds/lilly.mp3'),
@@ -8,26 +8,17 @@ new Audio('sounds/happy.mp3'), new Audio('sounds/hipHop.mp3'), new Audio('sounds
 new Audio('sounds/ocean.mp3')];
 
 function changeButton() {
-  play = !play;
-  if(play === true) {
-    this.innerHTML = '❚❚';
-  } else {
-    this.innerHTML = '►';
-  }
+  playButtons.forEach(button => button.innerHTML = '►');
+  currentButton = document.getElementById(this.id);
+  currentButton.innerHTML = '❚❚';
 }
 
 function transition() {
   music[currentSong].pause();
-  const previousButton = document.getElementById(currentSong);
-  if(play === true) {
-    previousButton.innerHTML = '❚❚';
-  } else {
-    previousButton.innerHTML = '►';
-  }
+  music[currentSong].currentTime = 0;
 }
 
 function togglePlay() {
-  changeButton();
   if(this.id !== currentSong && currentSong !== null) {
     transition();
   }
