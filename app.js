7a2a5b6617db1081afa6869bf07dc268b576ff6d
Justin Xia <justinxia9@gmail.com>
2021-09-12 12:15:30 -0400

    Fix display of play buttons

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const playButtons = document.querySelectorAll('.play');
 let currentSong = null;
 let currentButton;
+let playing = false;
 let currentlyPlaying;
 
 const music = [new Audio('sounds/melody.mp3'), new Audio('sounds/acoustic.mp3'), new Audio('sounds/cosmic.mp3'),
@@ -8,10 +9,19 @@ new Audio('sounds/inspiration.mp3'), new Audio('sounds/moonlight.mp3'), new Audi
 new Audio('sounds/happy.mp3'), new Audio('sounds/hipHop.mp3'), new Audio('sounds/rainy.mp3'),
 new Audio('sounds/ocean.mp3')];
 
+function switchButton() {
+  // playButtons.forEach(button => button.innerHTML = '►');
+  currentButton = document.getElementById(currentSong);
+  currentButton.innerHTML = '►';
+}
+
 function changeButton() {
-  playButtons.forEach(button => button.innerHTML = '►');
-  currentButton = document.getElementById(this.id);
-  currentButton.innerHTML = '❚❚';
+  playing = !playing;
+  if(playing === true) {
+    this.innerHTML = '❚❚';
+  } else {
+    this.innerHTML = '►';
+  }
 }
 
 function transition() {
@@ -26,12 +36,18 @@ function end(song) {
 }
 
 function togglePlay() {
+  playing = !playing;
+  changeButton();
   if(this.id !== currentSong && currentSong !== null) {
+    const btn = document.getElementById(this.id)
+    btn.innerHTML = '❚❚';
     transition();
+    switchButton();
   }
   const action = music[this.id].paused ? 'play' : 'pause';
   music[this.id][action]();
   currentSong = this.id;
+
   currentlyPlaying = music[this.id];
   end(currentlyPlaying);
 }
